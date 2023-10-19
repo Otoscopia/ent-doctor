52bd638717664ff6921bc6fca250b415f9d5cbeb
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-10-20 04:43:41 +0800

    feat: Add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,82 +1,25 @@
-"use client";
-import { useState } from "react";
+import Link from "next/link";
 import Footer from "./components/footer";
 
-export default function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [rememberMe, setRememberMe] = useState(false);
-
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-
-  const handleRememberMeChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRememberMe(event.target.checked);
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // TODO: Implement sign in logic
-  };
-
+export default function LandingPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex flex-1 flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold mb-8">Sign In</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <label htmlFor="email" className="text-lg font-medium">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={handleEmailChange}
-            className="border border-gray-300 rounded-lg px-4 py-2"
-            required
-          />
-          <label htmlFor="password" className="text-lg font-medium">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={handlePasswordChange}
-            className="border border-gray-300 rounded-lg px-4 py-2"
-            required
-          />
-          <div className="flex items-center gap-2">
-            <input
-              type="checkbox"
-              id="rememberMe"
-              name="rememberMe"
-              checked={rememberMe}
-              onChange={handleRememberMeChange}
-              className="rounded"
-            />
-            <label htmlFor="rememberMe" className="text-lg font-medium">
-              Remember me
-            </label>
-          </div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">Otoscopia</h1>
+
+        <p className="mt-3 text-2xl">Ear Screening Telemedicine Application</p>
+
+        <div className="mt-10">
           <button
-            type="submit"
+            type="button"
             className="bg-blue-500 text-white rounded-lg px-4 py-2"
           >
-            Sign In
+            <Link href="/sign-in">Get Started</Link>
           </button>
-        </form>
-      </div>
+        </div>
+      </main>
+
       <Footer />
-    </main>
+    </div>
   );
 }
