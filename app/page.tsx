89faee1efd0a3d471c3bf82aecb5461f7d7fd6a7
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-22 01:47:38 +0800

    fix: Remove svg warning on fill-rule & use fillRule. chore: Change sign-in.svg to welcome.svg

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Pikachu from "@/public/pikachu-sign-in.svg";
+import Pikachu from "@/public/pikachu-welcome.svg";
 
 export default function LandingPage() {
   return (
@@ -26,7 +26,7 @@ export default function LandingPage() {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
                 clipRule="evenodd"
               ></path>
