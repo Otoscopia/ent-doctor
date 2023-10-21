f8ff8a09f35a4670fa55905e2658712f4a545e8c
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-21 15:52:36 +0800

    feat: Export all icons

---
+++ icons/index.ts
@@ -0,0 +1,6 @@
+import FunnelIcon from "./funnel-icon";
+import PersonIcon from "./person-icon";
+import RefreshIcon from "./refresh-icon";
+import SearchIcon from "./search-icon";
+
+export { SearchIcon, FunnelIcon, RefreshIcon, PersonIcon };
