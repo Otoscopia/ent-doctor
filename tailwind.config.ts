9edde27f2407fd367fcc7de55b41192a884791a1
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-22 01:48:07 +0800

    feat: Initialize shadcn/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,32 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
 const themeColor = require("./utils/colors.ts");
 
-const config: Config = {
+module.exports = {
   darkMode: "media",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
       colors: themeColor,
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
-export default config;
