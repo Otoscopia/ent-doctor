c654ff2c67922ec496c9e47945d67ef293416181
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-22 01:43:44 +0800

    docs: Add env object

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    PROJECT_ENDPOINT: process.env.PROJECT_ENDPOINT,
+    PROJECT_ID: process.env.PROJECT_ID,
+    DATABASE_ID: process.env.DATABASE_ID,
+    PATIENT_COLLECTION: process.env.PATIENT_COLLECTION,
+    SCREENING_COLLECTION: process.env.SCREENING_COLLECTION,
+    NURSE_COLLECTION: process.env.NURSE_COLLECTION,
+    FOLLOW_UPS_COLLECTION: process.env.FOLLOW_UPS_COLLECTION,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
