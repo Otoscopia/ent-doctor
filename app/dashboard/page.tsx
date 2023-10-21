9dc282cd9a60eec821d8d444febbc767c4925976
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-21 16:58:23 +0800

    feat: Add flex to the content

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,28 @@
-import { TableBody, TableHeader, TableNav, TableTitles } from "@/components";
+import {
+  EmptyTable,
+  TableBody,
+  TableHeader,
+  TableNav,
+  TableTitles,
+} from "@/components";
+
+const table = [];
 
 export default async function Dashboard() {
   return (
-    <section className="py-3 sm:py-5 max-w-full w-full bg-pink-400">
-      <div className="px-4 mx-auto max-w-screen-2xl">
-        <div className="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+    <section className="py-3 sm:py-5 max-w-full min-h-screen">
+      <div className="mx-auto max-w-screen-2xl h-full flex flex-col items-center justify-center">
+        <div className="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg flex flex-col w-full">
           <TableHeader />
           <div className="overflow-x-auto">
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <TableTitles />
-              <TableBody />
-            </table>
+            {table.length > 0 ? (
+              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <TableTitles />
+                <TableBody />
+              </table>
+            ) : (
+              <EmptyTable />
+            )}
           </div>
           <TableNav />
         </div>
