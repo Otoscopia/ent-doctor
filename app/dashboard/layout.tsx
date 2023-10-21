2fb41cdd526402a8fdb3a121881abcba8221efed
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-22 01:50:56 +0800

    feat: dashboard is now a hidden url if user is not authenticated

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,45 @@
-import { Sidebar } from "@/components";
-import React, { ReactNode } from "react";
+"use client";
+
+import { Sidebar } from "@/app/components";
+import React, { ReactNode, useEffect, useState } from "react";
+import { getSession } from "../api/auth/routes";
+import { useRouter } from "next/navigation";
+import Loading from "../loading";
+import { getAccount } from "../api/appwrite";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
+  const { push } = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      try {
+        const session = await getSession();
+        if (!session) {
+          push("/auth");
+          localStorage.clear();
+        } else {
+          const user = getAccount();
+          if (!(await user).labels.includes("doctor")) {
+            push("/auth");
+            localStorage.clear();
+            throw new Error("You are not a doctor");
+          }
+          setLoading(false);
+        }
+      } catch (error) {
+        push("/auth");
+        setLoading(false);
+      }
+    };
+
+    checkSession();
+  }, [push]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <section className="flex flex-row max-w-full container">
       <Sidebar />
