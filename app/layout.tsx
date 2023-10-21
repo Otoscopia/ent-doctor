e07a80112544861a7f277158297407a188b838b7
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-22 01:49:40 +0800

    feat: Add Toaster provider for toast component of shadcn/ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "animate.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <main className="flex flex-col min-h-screen max-w-full">
           {children}
         </main>
+        <Toaster />
       </body>
     </html>
   );
