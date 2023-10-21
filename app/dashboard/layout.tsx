1d37e62934f8d3c01870b44719d5a5a396930037
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-21 15:48:26 +0800

    feat: Create dashboard layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from "@/components";
+import React, { ReactNode } from "react";
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <section className="flex flex-row max-w-full container">
+      <Sidebar />
+      <div className="w-full min-h-screen">{children}</div>
+    </section>
+  );
+}
