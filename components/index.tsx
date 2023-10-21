d6769e5482dec663fe87bc8c79d2ef5bd3c7bc40
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-21 17:21:51 +0800

    feat: Export all components

---
+++ components/index.tsx
@@ -0,0 +1,22 @@
+import Sidebar from "./sidebar";
+
+import FilterStatus from "./table/filter-status";
+import RefreshButton from "./table/refresh-button";
+import SearchInput from "./table/search-input";
+import TableBody from "./table/table-body";
+import TableHeader from "./table/table-header";
+import TableNav from "./table/table-nav";
+import TableTitles from "./table/table-titles";
+import EmptyTable from "./table/empty-table";
+
+export {
+  Sidebar,
+  FilterStatus,
+  RefreshButton,
+  SearchInput,
+  TableBody,
+  TableHeader,
+  TableNav,
+  TableTitles,
+  EmptyTable,
+};
