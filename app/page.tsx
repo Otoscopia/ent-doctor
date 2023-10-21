f96bde1d246544e5cec97bfd922f864a498d456f
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-21 17:17:50 +0800

    feat: Change UI layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,52 @@
+import Image from "next/image";
 import Link from "next/link";
-import Footer from "./components/footer";
+import Pikachu from "@/public/pikachu-sign-in.svg";
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">Otoscopia</h1>
-
-        <p className="mt-3 text-2xl">Ear Screening Telemedicine Application</p>
-
-        <div className="mt-10">
-          <button
-            type="button"
-            className="bg-blue-500 text-white rounded-lg px-4 py-2"
+    <section className="flex flex-col items-center justify-center w-full min-h-screen">
+      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white animate__animated animate__fadeInLeft animate_slow">
+            Effortlessly Access and Manage Your Medical Records
+          </h1>
+          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 animate_animated animate__fadeInUp">
+            With Otoscopia, medical records can be accessed and managed on the
+            go, making it a valuable tool for school healthcare professionals.
+          </p>
+          <Link
+            href="#"
+            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-800 hover:bg-primary-1600 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
           >
-            <Link href="/sign-in">Get Started</Link>
-          </button>
+            Get started
+            <svg
+              className="w-5 h-5 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </Link>
+          <Link
+            href="/auth"
+            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+          >
+            Sign In
+          </Link>
         </div>
-      </main>
-
-      <Footer />
-    </div>
+        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <Image
+            src={Pikachu}
+            alt="Pikachu Welcomes you"
+            className="animate_animated animate__fadeInRight animate_slower"
+          />
+        </div>
+      </div>
+    </section>
   );
 }
