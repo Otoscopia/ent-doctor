f5a1193c9a8560ff8aa0fff4dddcadae5250691a
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-29 23:31:56 +0800

    feat: Create general settings constants

---
+++ lib/core/constants/general_settings_constant.dart
@@ -0,0 +1,28 @@
+const String general = "General";
+
+const String notification = "Notification";
+const String notifyNewPatient = "Notify me when there is a new patient";
+const String notifyNewRecord =  "Notify me when there is a new medical record";
+const String notifyNewUpdate =  "Notify me when there is a new update";
+
+const String updateText = "Update";
+
+const String appearanceText = "Appearance";
+
+const String themeText = "Theme";
+const String themeLightText = "Light Mode";
+const String themeDarkText = "Dark Mode";
+const String themeSystemText = "System Theme";
+
+const String themeTextSubtitle = "Customize how you theme looks and feels.";
+
+const String accentColorText = "Accent Color";
+const String accentColorCurrent = 'Primary Color';
+const String customizeAccentColorText = "Customize the color of your theme.";
+
+const String languageText = "Language";
+const String languageSubtitleText = "Customize the langauge of the application.";
+const String languageCurrent = "English";
+
+const String authentication = "Authentication";
+const String applicationSecure = "Keep the application secured by enabling the automatic lock.";
