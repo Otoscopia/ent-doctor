256379c629e9732e4295950d576c5a2e1352b4ec
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-29 23:33:27 +0800

    chore: Use robohash network for random image

--- lib/feature/app/presentation/widgets/personal_info/profile.dart
+++ lib/feature/app/presentation/widgets/personal_info/profile.dart
@@ -1,6 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:ent_doctors/core/core.dart';
 import 'package:styled_widget/styled_widget.dart';
@@ -20,7 +19,7 @@ class ProfileContainer extends ConsumerWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
             children: [
-              ClipOval(child: SvgPicture.asset('default-profile.svg', width: 100, height: 100, fit: BoxFit.cover)),
+              ClipOval(child: Image.network("https://robohash.org/testing?set=set4", width: 100, height: 100, fit: BoxFit.cover)),
               mediumHeight,
               Text(profilePictureText, style: FluentTheme.of(context).typography.bodyStrong),
               mediumHeight,
