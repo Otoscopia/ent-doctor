489ec3a36d13523eafbf8e3a32d732b120fb031c
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-29 19:00:37 +0800

    feat: Add privacy and policy constants

---
+++ lib/core/constants/privacy_and_policy_constants.dart
@@ -0,0 +1,79 @@
+const String policyTitle = "Policy";
+const String privacyTitle = "Privacy";
+const String privacyAcknowledgeTitle = "By using the Application, you acknowledge that you have read, understood, and agreed to these terms and conditions. If you do not agree to these terms and conditions, you should not use the Application.";
+const String privacyUpdatedTitle = "Last updated: 10/29/2023";
+
+List<Map<String, String>> policies = [
+  {
+    "title": "1. Personal Identification Information:",
+    "description":
+        """    We may collect personal identification information from Users in various ways, including but not limited to when Users visit the Application, register on the Application, fill out a form, and in connection with other activities, services, features, or resources we make available on our Application. Users may be asked for, as appropriate, name, email address, phone number, and other relevant information. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personal identification information, except that it may prevent them from engaging in certain Application-related activities."""
+  },
+  {
+    "title": "2. Non-Personal Identification Information:",
+    "description":
+        """    We may collect non-personal identification information about Users whenever they interact with our Application. Non-personal identification information may includes the type of device, and technical information about Users' means of connection to our Application, such as the operating system and Internet service providers utilized, and other similar information."""
+  },
+  {
+    "title": "3. How We Use Collected Information:",
+    "description":
+        """    We may collect and use Users' personal information for the following purposes:
+          1. To improve customer service: Information provided helps us respond to customer service requests and support needs more efficiently.
+          2. To personalize user experience: We may use information in the aggregate to understand how our Users as a group use the services and resources provided on our Application.
+          3. To send periodic emails: We may the email address to respond to inquiries, questions, and/or other requests from Users."""
+  },
+  {
+    "title": "4. Protection of Information:",
+    "description":
+        """    We adopt appropriate data collection, storage, and processing practices and security measures to protect against unauthorized access, alteration, disclosure, or destruction of Users' personal information and data stored on our Application."""
+  },
+  {
+    "title": "5. Sharing Personal Information:",
+    "description":
+        """    We do not sell, trade, or rent Users' personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding Users with our partners, trusted affiliates, and advertisers for the purposes outlined above."""
+  },
+  {
+    "title": "6. Changes to This Privacy Policy:",
+    "description":
+        """    We have the discretion to update this Privacy Policy at any time. When we do, we will revise the updated date at the bottom of this page. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this Privacy Policy periodically and become aware of modifications."""
+  },
+];
+
+const String policy =
+    'This Privacy Policy governs the manner in which we collect, use, maintain, and disclose information collected from users (hereinafter referred to as "User") of our mobile ear health application Otoscopia. This Privacy Policy applies to the Application and all products and services offered by us.';
+
+List<Map<String, String>> privacies = [
+  {
+    "title": "1. Use of the Application:",
+    "description":
+        """    The Application is intended to provide educational and informational content related to ear health. The information provided within the Application is not a substitute for professional medical advice, diagnosis, or treatment. You understand and acknowledge that the Application is not intended to replace the services of qualified healthcare professionals."""
+  },
+  {
+    "title": "2. Intellectual Property:",
+    "description":
+        """    The Application and all of its contents, including but not limited to text, graphics, logos, button icons, images, audio clips, and software, are the property of the Philippine Otolaryngology Society, the Department of Education, and Mindanao State University - Iligan Institute of Technology or their respective licensors, and are protected by copyright and other intellectual property laws. You may not modify, publish, transmit, distribute, perform, display, or sell any of the content or functionality of the Application."""
+  },
+  {
+    "title": "3. User Conduct:",
+    "description":
+        """    You agree to use the Application in compliance with all applicable laws and regulations. You shall not engage in any activity that may interfere with or disrupt the functioning of the Application or any servers or networks connected to the Application."""
+  },
+  {
+    "title": "4. Disclaimer of Warranties:",
+    "description":
+        """    The Application is provided on an "as is" and "as available" basis, without any warranties of any kind, either expressed or implied, including but not limited to warranties of accuracy, reliability, merchantability, fitness for a particular purpose, or non-infringement. The Philippine Otolaryngology Society, the Department of Education and Mindanao State University - Iligan Institute of Technology do not warrant that the Application will be error-free or uninterrupted, or that any defects in the Application will be corrected."""
+  },
+  {
+    "title": "5. Limitation of Liability:",
+    "description":
+        """    In no event shall the Philippine Otolaryngology Society, the Department of Education, the Mindanao State University - Iligan Institute of Technology or their respective officers, directors, employees, or agents be liable for any direct, indirect, incidental, special, or consequential damages arising out of or in connection with the use or inability to use the Application, even if advised of the possibility of such damages."""
+  },
+  {
+    "title": "6. Governing Law and Jurisdiction:",
+    "description":
+        """    These terms and conditions shall be governed by and construed in accordance with the laws of the Philippines. Any dispute arising out of or in connection with these terms and conditions shall be subject to the exclusive jurisdiction of the courts of the Philippines."""
+  },
+];
+
+const String privacy =
+    'By downloading, installing, and using our mobile ear health application Otoscopia, you agree to be bound by the following terms and conditions:';
