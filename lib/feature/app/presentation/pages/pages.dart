7a1114321f24f0eb60979344e14abfc6717957a3
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-29 20:05:01 +0800

    feat: Export general settings page

--- lib/feature/app/presentation/pages/pages.dart
+++ lib/feature/app/presentation/pages/pages.dart
@@ -2,5 +2,6 @@ export './dashboard.dart';
 export './home.dart';
 export './settings.dart';
 
+export 'settings/general_settings.dart';
 export 'settings/privacy_and_policy.dart';
 export 'settings/personal_info.dart';
