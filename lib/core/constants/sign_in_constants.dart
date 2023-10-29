d591043e90d6418c1b8c6969aa386ded81683867
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-29 08:50:28 +0800

    chore: Add more sign in constants

--- lib/core/constants/sign_in_constants.dart
+++ lib/core/constants/sign_in_constants.dart
@@ -1,4 +1,11 @@
 const String logoSvg = 'assets/logo.svg';
+
 const String emailAddressText = 'Email Address';
+const String emailAddressLabel = emailAddressText;
+const String emailAddressError = 'Please enter valid email address';
+
 const String passwordText = 'Password';
+const String passwordLabel = passwordText;
+const String passwordError = 'Please enter a valid password';
+
 const String signInText = 'Sign In';
