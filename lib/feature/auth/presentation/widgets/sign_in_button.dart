4b45f1686f88940afcf51219b2895ff84a4ef318
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-29 08:57:05 +0800

    feat: Create sign in button

---
+++ lib/feature/auth/presentation/widgets/sign_in_button.dart
@@ -0,0 +1,18 @@
+import 'package:ent_doctors/core/constants/sign_in_constants.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+class SignInButton extends ConsumerWidget {
+  const SignInButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SizedBox(
+      width: 150,
+      child: FilledButton(
+          child: const Text(signInText).fontWeight(FontWeight.w500),
+          onPressed: () {}),
+    );
+  }
+}
