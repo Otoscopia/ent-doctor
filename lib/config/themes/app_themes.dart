f1e009f7b3aeaaf72f10fd65dee31d3948fed13b
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-29 18:42:47 +0800

    feat: Application font now have default size of 16

--- lib/config/themes/app_themes.dart
+++ lib/config/themes/app_themes.dart
@@ -10,6 +10,16 @@ final FluentThemeData lightTheme = FluentThemeData(
   accentColor: accentColor,
   scaffoldBackgroundColor: lightBackgroundAccent,
   cardColor: lightBackgroundAccent,
+  typography: Typography.raw(
+    display: DarkTextStyles().display,
+    titleLarge: DarkTextStyles().titleLarge,
+    title: DarkTextStyles().title,
+    subtitle: DarkTextStyles().subtitle,
+    bodyLarge: DarkTextStyles().bodyLarge,
+    bodyStrong: DarkTextStyles().bodyStrong,
+    body: DarkTextStyles().body,
+    caption: DarkTextStyles().caption,
+  ),
 );
 
 final FluentThemeData darkTheme = FluentThemeData(
@@ -18,4 +28,101 @@ final FluentThemeData darkTheme = FluentThemeData(
   scaffoldBackgroundColor: darkBackground,
   cardColor: darkBackgroundAccent,
   fontFamily: "Quicksand",
+  typography: Typography.raw(
+    display: LightTextStyles().display,
+    titleLarge: LightTextStyles().titleLarge,
+    title: LightTextStyles().title,
+    subtitle: LightTextStyles().subtitle,
+    bodyLarge: LightTextStyles().bodyLarge,
+    bodyStrong: LightTextStyles().bodyStrong,
+    body: LightTextStyles().body,
+    caption: LightTextStyles().caption,
+  ),
 );
+
+class DarkTextStyles {
+  final display = const TextStyle(
+    fontSize: 72,
+    color: Color(0xE4000000),
+    fontWeight: FontWeight.w600,
+  );
+  final titleLarge = const TextStyle(
+    fontSize: 44,
+    color: Color(0xE4000000),
+    fontWeight: FontWeight.w600,
+  );
+  final title = const TextStyle(
+    fontSize: 32,
+    color: Color(0xE4000000),
+    fontWeight: FontWeight.w600,
+  );
+  final subtitle = const TextStyle(
+    fontSize: 24,
+    color: Color(0xE4000000),
+    fontWeight: FontWeight.w600,
+  );
+  final bodyLarge = const TextStyle(
+    fontSize: 22,
+    color: Color(0xE4000000),
+    fontWeight: FontWeight.normal,
+  );
+  final bodyStrong = const TextStyle(
+    fontSize: 18,
+    color: Color(0xE4000000),
+    fontWeight: FontWeight.w600,
+  );
+  final body = const TextStyle(
+    fontSize: 18,
+    color: Color(0xE4000000),
+    fontWeight: FontWeight.normal,
+  );
+  final caption = const TextStyle(
+    fontSize: 16,
+    color: Color(0xE4000000),
+    fontWeight: FontWeight.normal,
+  );
+}
+
+class LightTextStyles {
+  final display = const TextStyle(
+    fontSize: 72,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+  );
+
+  final titleLarge = const TextStyle(
+    fontSize: 44,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+  );
+  final title = const TextStyle(
+    fontSize: 32,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+  );
+  final subtitle = const TextStyle(
+    fontSize: 24,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+  );
+  final bodyLarge = const TextStyle(
+    fontSize: 22,
+    color: Colors.white,
+    fontWeight: FontWeight.normal,
+  );
+  final bodyStrong = const TextStyle(
+    fontSize: 18,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+  );
+  final body = const TextStyle(
+    fontSize: 18,
+    color: Colors.white,
+    fontWeight: FontWeight.normal,
+  );
+  final caption = const TextStyle(
+    fontSize: 16,
+    color: Colors.white,
+    fontWeight: FontWeight.normal,
+  );
+}
