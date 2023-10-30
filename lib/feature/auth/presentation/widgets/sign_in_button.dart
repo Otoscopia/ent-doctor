59d391334ecc3003692d6ac026101e6fde27f6a7
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-30 14:34:03 +0800

    feat: Add Navigation push to dashboard

--- lib/feature/auth/presentation/widgets/sign_in_button.dart
+++ lib/feature/auth/presentation/widgets/sign_in_button.dart
@@ -1,4 +1,5 @@
 import 'package:ent_doctors/core/constants/sign_in_constants.dart';
+import 'package:ent_doctors/feature/app/app.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:styled_widget/styled_widget.dart';
@@ -12,7 +13,7 @@ class SignInButton extends ConsumerWidget {
       width: 150,
       child: FilledButton(
           child: const Text(signInText).fontWeight(FontWeight.w500),
-          onPressed: () {}),
+          onPressed: () => Navigator.push(context, FluentPageRoute(builder: (context) => const Dashboard()))),
     );
   }
 }
