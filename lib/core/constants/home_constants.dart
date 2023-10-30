6012ee756678990f2e307bcda4d7c8a092ba94a8
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-30 14:06:42 +0800

    feat: Create home constants

---
+++ lib/core/constants/home_constants.dart
@@ -0,0 +1,17 @@
+const String searchPatientText = "Search Patients";
+const String refreshText = "Refresh";
+
+const String filterByText = "Filter By: ";
+const String filterStatus = "Status";
+const String filterByDefaultText = "Default";
+const String filterByFinalText = "Final";
+const String filterByMedicalText = "Medical";
+const String filterByDiagnosisText = "Diagnosis";
+
+const List<String> tableHeader = [
+  'Name',
+  'Age/Gender',
+  'Status',
+  'Uploaded Date',
+  'Screened By',
+];
