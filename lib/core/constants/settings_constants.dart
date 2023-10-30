a1b8a76e6aad442630ce34ba3abdd2608e5fe886
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-30 14:10:12 +0800

    feat: Create settings tabs for navigation items

--- lib/core/constants/settings_constants.dart
+++ lib/core/constants/settings_constants.dart
@@ -1,4 +1,26 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:ent_doctors/feature/app/app.dart';
+
 const String settingsTitle = 'Settings';
 const String generalTitle = 'General';
 const String personalInfoTitle = 'Personal Info';
 const String privacyAndPolicyTitle = 'Privacy & Policy';
+
+List<NavigationPaneItem> settingsTabs = [
+  PaneItem(
+      icon: const Icon(FluentIcons.default_settings),
+      title: const Text(generalTitle),
+      body: const GeneralSettings()),
+  PaneItemSeparator(),
+  PaneItem(
+    icon: const Icon(FluentIcons.player_settings),
+    title: const Text(personalInfoTitle),
+    body: const PersonalInfo(),
+  ),
+  PaneItemSeparator(),
+  PaneItem(
+    icon: const Icon(FluentIcons.settings_secure),
+    title: const Text(privacyAndPolicyTitle),
+    body: const PrivacyAndPolicy(),
+  ),
+];
