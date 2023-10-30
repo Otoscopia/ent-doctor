2f4a91aff03d78dcd58f673372c989a0a3c14a90
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-30 13:28:49 +0800

    feat: Create patient information constants

---
+++ lib/core/constants/patient_information_constants.dart
@@ -0,0 +1,41 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:ionicons/ionicons.dart';
+
+const String vitalsTitle = "Vitals";
+
+const List<Icon> icons = [
+  Icon(Ionicons.barbell),
+  Icon(Ionicons.body),
+  Icon(Ionicons.thermometer)
+];
+
+const List<String> vitalTitles = [
+  "Weight",
+  "Height",
+  "Temperature",
+];
+
+const String birthdayText = "Birthday: ";
+const String ageGenderText = "Age/Gender:";
+const String schoolText = "School:";
+const String contactNumberText = "Contact Number:";
+
+const String healthRecordText = "Health Record";
+const String performedByText = "Perfomed By:";
+const String performedAtText = "Performed At:";
+
+const String healthWorkerRemarksText = "Health Worker Remarks:";
+const String chiefComplaintText = "Chief Complaint:";
+const String chiefComplaintMessageText = "Chief Complaint Message:";
+const String historyOfIllnessText = "History of Illness:";
+
+const String diagnosisText = "Diagnosis";
+const String patientTreatedText = "Has the patient been treated for a similar condition or had any injuries to the same area in the past?";
+const String patientAllergiesText = "Does the patient have any allergies?";
+const String patientSurgicalProcedureText = "Does the patient undergo any surgical procedure in the past?";
+const String patientMedicationText = "Does the patient currently taking any medication?";
+const String patientMedicationTypeText = "Patient medication type:";
+
+
+const String leftImagesText = "Left Images";
+const String rightImagesText = "Right Images";
