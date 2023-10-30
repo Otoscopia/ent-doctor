0226029e32c162c73fa928faa2804e16c7f2c483
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-30 14:08:20 +0800

    feat: Export more widgets

--- lib/feature/app/presentation/widgets/widgets.dart
+++ lib/feature/app/presentation/widgets/widgets.dart
@@ -1,7 +1,14 @@
 export './privacy_and_policy_widget.dart';
+
 export './personal_info/personal_info.dart';
+
 export './patient_info/vitals_container.dart';
 export './patient_info/patient_card.dart';
 export './patient_info/health_record.dart';
 export './patient_info/diagnosis.dart';
 export './patient_info/screening_images.dart';
+
+export 'home/filter_button.dart';
+export 'home/search_box.dart';
+export 'home/refresh_button.dart';
+export 'home/patient_table.dart';
